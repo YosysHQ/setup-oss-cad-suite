800bc4af6e127133d9bdf20127fb52d65309beb2
Aki Van Ness <aki@lethalbit.net>
2023-02-10 18:11:08 -0500

    dist: updated package

--- dist/index.js
+++ dist/index.js
@@ -50,19 +50,25 @@ function getDownloadURL(platform = 'linux', arch = 'x64', tag) {
         const ARCHIVE_PREFIX = `oss-cad-suite-${platform}-${arch}`;
         const API_ENDPOINT = (() => {
             if (tag) {
-                return `releases/tag/${tag}`;
+                return `releases/tags/${tag}`;
             }
             return 'releases/latest';
         })();
+        const ENDPOINT_URL = `${API_URL}/${API_ENDPOINT}`;
+        core.debug(`Using API_ENDPOINT of '${API_ENDPOINT}'`);
+        core.debug(`Archive prefix is '${ARCHIVE_PREFIX}'`);
         const _http = new http.HttpClient(`setup-oss-cad-suite-v${process.env.npm_package_version}`);
         core.info(`Getting download URL for ${ARCHIVE_PREFIX}`);
-        const resp = yield _http.getJson(`${API_URL}/${API_ENDPOINT}`);
+        core.debug(`Endpoint URL is '${ENDPOINT_URL}'`);
+        const resp = yield _http.getJson(ENDPOINT_URL);
         const assets = (_a = resp.result) === null || _a === void 0 ? void 0 : _a.assets;
         if (!assets) {
+            core.debug('assets is empty');
             throw Error('Unable to get download URL for oss-cad-suite package');
         }
         const url = assets.filter(pkg => pkg.name.startsWith(ARCHIVE_PREFIX)).map(pkg => pkg.browser_download_url).shift();
         if (!url) {
+            core.debug('Could not find download URL for prefixed package');
             throw Error('Unable to get download URL for oss-cad-suite package');
         }
         return url;
@@ -92,16 +98,22 @@ function main() {
             const os = process.platform;
             const arch = process.arch;
             const tag = core.getInput('version');
+            core.debug(`Package directory is '${pkg_dir}'`);
+            // Ensure we're running on a supported configuration
             _validate(os, arch);
             // Make the target dir for extraction
             yield io.mkdirP(pkg_dir);
             // Get the download URL for the package and then download it
             const download_url = yield getDownloadURL(os, arch, tag === '' ? undefined : tag);
+            // Download the package to the temp directory
             core.info(`Downloading package from ${download_url}`);
             const pkg_file = yield tc.downloadTool(download_url, `${process.env.RUNNER_TEMP}/oss-cad-suite.tgz`);
+            // Extract the package
             core.info(`Extracting ${pkg_file} to ${pkg_dir}`);
             const suite_path = yield tc.extractTar(pkg_file, pkg_dir, ['xz', '--strip-components=1']);
+            // Add the bin dir to the path
             core.addPath(`${suite_path}/bin`);
+            // If we are overloading the system python, do so
             if (core.getInput('python-override')) {
                 core.info('Overloading system python with oss-cad-suite provided python');
                 core.addPath(`${suite_path}/py3bin`);
