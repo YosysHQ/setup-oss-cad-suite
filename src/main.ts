4eaf77b6eef03f9251a11b00a7a201bc197453ce
Aki Van Ness <aki@lethalbit.net>
2023-02-10 18:10:42 -0500

    action: added a bunch of additional logging

--- src/main.ts
+++ src/main.ts
@@ -23,15 +23,20 @@ async function getDownloadURL(platform = 'linux', arch = 'x64', tag?: string): P
 		}
 		return 'releases/latest'
 	})()
+	const ENDPOINT_URL = `${API_URL}/${API_ENDPOINT}`
+
+	core.debug(`Using API_ENDPOINT of '${API_ENDPOINT}'`)
+	core.debug(`Archive prefix is '${ARCHIVE_PREFIX}'`)
 
 	const _http = new http.HttpClient(`setup-oss-cad-suite-v${process.env.npm_package_version}`)
 
 	core.info(`Getting download URL for ${ARCHIVE_PREFIX}`)
-
-	const resp = await _http.getJson<github_release>(`${API_URL}/${API_ENDPOINT}`)
+	core.debug(`Endpoint URL is '${ENDPOINT_URL}'`)
+	const resp = await _http.getJson<github_release>(ENDPOINT_URL)
 	const assets = resp.result?.assets
 
 	if (!assets) {
+		core.debug('assets is empty')
 		throw Error('Unable to get download URL for oss-cad-suite package')
 	}
 
@@ -42,6 +47,7 @@ async function getDownloadURL(platform = 'linux', arch = 'x64', tag?: string): P
 	).shift()
 
 	if (!url) {
+		core.debug('Could not find download URL for prefixed package')
 		throw Error('Unable to get download URL for oss-cad-suite package')
 	}
 	return url
@@ -70,6 +76,9 @@ async function main(): Promise<void> {
 		const arch = process.arch
 		const tag = core.getInput('version')
 
+		core.debug(`Package directory is '${pkg_dir}'`)
+
+		// Ensure we're running on a supported configuration
 		_validate(os, arch)
 
 		// Make the target dir for extraction
@@ -78,19 +87,26 @@ async function main(): Promise<void> {
 		// Get the download URL for the package and then download it
 		const download_url = await getDownloadURL(os, arch, tag === '' ? undefined : tag)
 
+		// Download the package to the temp directory
 		core.info(`Downloading package from ${download_url}`)
 		const pkg_file = await tc.downloadTool(download_url, `${process.env.RUNNER_TEMP}/oss-cad-suite.tgz`)
 
+		// Extract the package
 		core.info(`Extracting ${pkg_file} to ${pkg_dir}`)
 		const suite_path = await tc.extractTar(
 			pkg_file, pkg_dir,
 			['xz', '--strip-components=1']
 		)
+
+		// Add the bin dir to the path
 		core.addPath(`${suite_path}/bin`)
+
+		// If we are overloading the system python, do so
 		if (core.getInput('python-override')) {
 			core.info('Overloading system python with oss-cad-suite provided python')
 			core.addPath(`${suite_path}/py3bin`)
 		}
+
 		core.info('Done')
 	} catch (err) {
 		if (err instanceof Error) {
